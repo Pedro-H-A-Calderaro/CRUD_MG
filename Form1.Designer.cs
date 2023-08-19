11ada6634840cbecd01b6101b25f1f0fceda6325
User <User@DESKTOP-3E0TNIK>
2023-08-19 18:22:52 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace CRUD_MG
+{
+    partial class AgendaContato
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AgendaContato));
+            this.btn_Inserir = new System.Windows.Forms.Button();
+            this.btn_Editar = new System.Windows.Forms.Button();
+            this.btn_Remover = new System.Windows.Forms.Button();
+            this.btn_Consutar = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Salvar = new System.Windows.Forms.Button();
+            this.btn_Cancelar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Inserir
+            // 
+            this.btn_Inserir.Location = new System.Drawing.Point(12, 12);
+            this.btn_Inserir.Name = "btn_Inserir";
+            this.btn_Inserir.Size = new System.Drawing.Size(75, 23);
+            this.btn_Inserir.TabIndex = 0;
+            this.btn_Inserir.Text = "Inserir";
+            this.btn_Inserir.UseVisualStyleBackColor = true;
+            this.btn_Inserir.Click += new System.EventHandler(this.btn_Inserir_Click);
+            // 
+            // btn_Editar
+            // 
+            this.btn_Editar.Location = new System.Drawing.Point(93, 12);
+            this.btn_Editar.Name = "btn_Editar";
+            this.btn_Editar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Editar.TabIndex = 1;
+            this.btn_Editar.Text = "Editar";
+            this.btn_Editar.UseVisualStyleBackColor = true;
+            this.btn_Editar.Click += new System.EventHandler(this.btn_Editar_Click);
+            // 
+            // btn_Remover
+            // 
+            this.btn_Remover.Location = new System.Drawing.Point(174, 12);
+            this.btn_Remover.Name = "btn_Remover";
+            this.btn_Remover.Size = new System.Drawing.Size(75, 23);
+            this.btn_Remover.TabIndex = 2;
+            this.btn_Remover.Text = "Remover";
+            this.btn_Remover.UseVisualStyleBackColor = true;
+            this.btn_Remover.Click += new System.EventHandler(this.btn_Remover_Click);
+            // 
+            // btn_Consutar
+            // 
+            this.btn_Consutar.Location = new System.Drawing.Point(255, 12);
+            this.btn_Consutar.Name = "btn_Consutar";
+            this.btn_Consutar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Consutar.TabIndex = 3;
+            this.btn_Consutar.Text = "Consutar";
+            this.btn_Consutar.UseVisualStyleBackColor = true;
+            this.btn_Consutar.Click += new System.EventHandler(this.btn_Consutar_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 69);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(318, 20);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(15, 127);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(315, 20);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(15, 186);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Telefone";
+            // 
+            // btn_Salvar
+            // 
+            this.btn_Salvar.Location = new System.Drawing.Point(13, 237);
+            this.btn_Salvar.Name = "btn_Salvar";
+            this.btn_Salvar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Salvar.TabIndex = 11;
+            this.btn_Salvar.Text = "Salvar";
+            this.btn_Salvar.UseVisualStyleBackColor = true;
+            this.btn_Salvar.Click += new System.EventHandler(this.btn_Salvar_Click);
+            // 
+            // btn_Cancelar
+            // 
+            this.btn_Cancelar.Location = new System.Drawing.Point(93, 237);
+            this.btn_Cancelar.Name = "btn_Cancelar";
+            this.btn_Cancelar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancelar.TabIndex = 12;
+            this.btn_Cancelar.Text = "Cancelar";
+            this.btn_Cancelar.UseVisualStyleBackColor = true;
+            this.btn_Cancelar.Click += new System.EventHandler(this.btn_Cancelar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(336, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 288);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AgendaContato
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(625, 328);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_Cancelar);
+            this.Controls.Add(this.btn_Salvar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btn_Consutar);
+            this.Controls.Add(this.btn_Remover);
+            this.Controls.Add(this.btn_Editar);
+            this.Controls.Add(this.btn_Inserir);
+            this.Name = "AgendaContato";
+            this.Text = "Agenda Contato";
+            this.Load += new System.EventHandler(this.AgendaContato_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Inserir;
+        private System.Windows.Forms.Button btn_Editar;
+        private System.Windows.Forms.Button btn_Remover;
+        private System.Windows.Forms.Button btn_Consutar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Salvar;
+        private System.Windows.Forms.Button btn_Cancelar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CRUD_MG
+{
+    public partial class AgendaContato : Form
+    {
+        string connectionString = "Server=localhost;Database=serverBanco;Uid=root;Pwd=1911@JlPh;";
+        bool novo;
+
+        public AgendaContato()
+        {
+            InitializeComponent();
+        }
+
+        private void AgendaContato_Load(object sender, EventArgs e)
+        {
+            btn_Inserir.Enabled = true;
+            btn_Editar.Enabled = false;
+            btn_Remover.Enabled = false;
+            btn_Consutar.Enabled = true;
+            btn_Salvar.Enabled = false;
+            btn_Cancelar.Enabled = true;
+            txtNome.Enabled = true;
+            txtEmail.Enabled = false;
+            txtTelefone.Enabled = false;
+        }
+
+        /* Botão Inserir */
+        private void btn_Inserir_Click(object sender, EventArgs e)
+        {
+            btn_Inserir.Enabled = false;
+            btn_Editar.Enabled = true;
+            btn_Remover.Enabled = false;
+            btn_Consutar.Enabled = false;
+            btn_Salvar.Enabled = true;
+            btn_Cancelar.Enabled = true;
+            txtNome.Enabled = true;
+            txtEmail.Enabled = true;
+            txtTelefone.Enabled = true;
+            txtNome.Focus();
+            novo = true;
+        }
+
+        /* Botão Editar */
+        private void btn_Editar_Click(object sender, EventArgs e)
+        {
+            btn_Inserir.Enabled = false;
+            btn_Editar.Enabled = false;
+            btn_Remover.Enabled = false;
+            btn_Consutar.Enabled = false;
+            btn_Salvar.Enabled = true;
+            btn_Cancelar.Enabled = true;
+            txtNome.Enabled = false;
+            txtEmail.Enabled = true;
+            txtTelefone.Enabled = true;
+            novo = false;
+        }
+
+        /* Botão Salvar */
+        private void btn_Salvar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                if (novo)
+                {
+                    string sql = "INSERT INTO CLIENTE (NOME, EMAIL, TELEFONE) VALUES (@Nome, @Email, @Telefone)";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Nome", txtNome.Text);
+                        cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                        cmd.Parameters.AddWithValue("@Telefone", txtTelefone.Text);
+
+                        try
+                        {
+                            int linhasAfetadas = cmd.ExecuteNonQuery();
+                            if (linhasAfetadas > 0)
+                                MessageBox.Show("Cadastro salvo com sucesso");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Erro: " + ex.ToString());
+                        }
+                    }
+                }
+                else
+                {
+                    string sql = "UPDATE CLIENTE SET NOME=@Nome, EMAIL=@Email, TELEFONE=@Telefone WHERE NOME=@OldNome";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Nome", txtNome.Text);
+                        cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                        cmd.Parameters.AddWithValue("@Telefone", txtTelefone.Text);
+                        cmd.Parameters.AddWithValue("@OldNome", txtNome.Text);
+
+                        try
+                        {
+                            int linhasAfetadas = cmd.ExecuteNonQuery();
+                            if (linhasAfetadas > 0)
+                                MessageBox.Show("Cadastro atualizado com sucesso");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Erro: " + ex.ToString());
+                        }
+                        finally
+                        {
+                            con.Close();
+                        }
+                    }
+
+                    btn_Inserir.Enabled = true;
+                    btn_Editar.Enabled = false;
+                    btn_Remover.Enabled = false;
+                    btn_Consutar.Enabled = true;
+                    btn_Salvar.Enabled = false;
+                    btn_Cancelar.Enabled = false;
+                    txtNome.Enabled = true;
+                    txtEmail.Enabled = false;
+                    txtTelefone.Enabled = false;
+                }
+            }
+        }
+
+        /* Botão Cancelar */
+        private void btn_Cancelar_Click(object sender, EventArgs e)
+        {
+            btn_Inserir.Enabled = true;
+            btn_Editar.Enabled = false;
+            btn_Remover.Enabled = false;
+            btn_Consutar.Enabled = false;
+            btn_Salvar.Enabled = false;
+            btn_Cancelar.Enabled = false;
+            txtNome.Enabled = true;
+            txtEmail.Enabled = false;
+            txtTelefone.Enabled = false;
+            txtNome.Text = "";
+            txtEmail.Text = "";
+            txtTelefone.Text = "";
+        }
+
+        /* Botão Remover */
+        private void btn_Remover_Click(object sender, EventArgs e)
+        {
+            string sql = "DELETE FROM CLIENTE WHERE NOME=@Nome";
+
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                    MessageBox.Show("Registro excluído");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            btn_Inserir.Enabled = true;
+            btn_Editar.Enabled = false;
+            btn_Remover.Enabled = false;
+            btn_Consutar.Enabled = true;
+            btn_Salvar.Enabled = false;
+            btn_Cancelar.Enabled = false;
+            txtNome.Enabled = true;
+            txtEmail.Enabled = false;
+            txtTelefone.Enabled = false;
+            txtNome.Text = "";
+            txtEmail.Text = "";
+            txtTelefone.Text = "";
+        }
+
+        /* Botão Consutar*/
+        private void btn_Consutar_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT*FROM CLIENTE WHERE NOME=" + txtNome.Text;
+
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand (sql, con);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader reader;
+            con.Open();
+
+            try
+            {
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    btn_Inserir.Enabled = false;
+                    btn_Editar.Enabled = true;
+                    btn_Remover.Enabled = true;
+                    btn_Consutar.Enabled = false;
+                    btn_Salvar.Enabled = true;
+                    btn_Cancelar.Enabled = true;
+                    txtNome.Enabled = true;
+                    txtEmail.Enabled = true;
+                    txtTelefone.Enabled = true;
+                    txtNome.Focus();
+                    txtNome.Text = reader[0].ToString();
+                    txtEmail.Text = reader[1].ToString();
+                    txtTelefone.Text = reader[2].ToString();
+                    novo = false;
+                }
+                else
+                    MessageBox.Show("Nenhum registro encontrado com o Nome informado!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            txtNome.Text = "";
+
+            reader = cmd.ExecuteReader ();
+        }
+    }
+}
